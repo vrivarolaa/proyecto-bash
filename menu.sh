30e080d598f842ec3c5b7ba967571cd156648346
Martin Campuzano <mcampuzano@fi.uba.ar>
2026-09-02 11:23:38 -0300

    arreglo funciones, agrego validaciones

--- menu.sh
+++ menu.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 if [[ "$1" == "-d" ]]; then
-    pkill -f consolidar.sh     
-    cp "$HOME/EPNro1/consolidar.sh" ./ 2>/dev/null
-    cp "$HOME/EPNro1/procesado"/*.txt ./ 2>/dev/null
-    rm -rf "$HOME/EPNro1"        
+    pkill -f consolidar.sh
+    rm -rf "$HOME/EPNro1"
     exit 0
 fi
 
@@ -26,23 +24,24 @@ fi
 Mostrar_menu() {
     echo "1) Crear Entorno"
     echo "2) Correr Proceso"
-    echo "3) Mostrar Alumno"
+    echo "3) Mostrar Alumnos"
     echo "4) Mostrar las 10 notas mas altas"
     echo "5) Mostrar datos de un alumo"
     echo "6) Visualizar log"
     echo "7) Salir"
 }
 
-
+Crear_entorno() {
     mkdir -p "$HOME/EPNro1/entrada"
     mkdir -p "$HOME/EPNro1/salida"
     mkdir -p "$HOME/EPNro1/procesado"
     cp consolidar.sh "$HOME/EPNro1/" 
+    touch "$HOME/EPNro1/procesado.log"
     chmod +x "$HOME/EPNro1/consolidar.sh" 
 }
 
-correr_proceso(){
-    if [ -f "$HOME/EPNro1/consolidar.sh" ]; then
+Correr_proceso() {
+    if [ -e "$HOME/EPNro1" ]; then
         bash "$HOME/EPNro1/consolidar.sh" > /dev/null 2>&1 & 
     else
         echo " Falta crear el entorno"
@@ -50,41 +49,50 @@ correr_proceso(){
 }
 
 
-mostrar_alumnnos(){
+Mostrar_alumnos() {
     if [ -f "$HOME/EPNro1/salida/${FILENAME}.txt" ]; then
         echo "Alumnos ordenados segun padron: "
         sort -n < "$HOME/EPNro1/salida/${FILENAME}.txt"
+    else
+        echo "Aun no hay datos"
     fi
 }
 
 
-mostrar_notas_altas (){
+Mostrar_notas_altas() {
     if [ -f "$HOME/EPNro1/salida/${FILENAME}.txt" ]; then
         echo "Las 10 mejores notas"
         sort -n -r -k5 "$HOME/EPNro1/salida/${FILENAME}.txt" | head -n 10
-    fi
-}
-
-
-mostrar_datos (){
-    if [ -f "$HOME/EPNro1/salida/${FILENAME}.txt" ]; then
-        read -p "Ingrese el numero de padron del alumo: " padron
-        grep -E "^$padron\s" "$HOME/EPNro1/salida/${FILENAME}.txt"
     else
-        echo "no hay datos aun"
+	echo "aun no hay datos"
     fi
 }
 
-visualizar_log() {
-    if [ -f "$HOME/EPNro1/procesado.log" ]; then
+
+Buscar_por_padron() {
+    if [ ! -f "$HOME/EPNro1/salida/${FILENAME}.txt" ]; then
+        echo "no hay datos aun"
+    else
+	read -p "Ingrese el numero de padron del alumo: " padron
+	resultado=$(grep "^$padron " "$HOME/EPNro1/salida/${FILENAME}.txt")
+		if [ -n "$resultado" ]; then
+		    echo "$resultado"
+		else
+		    echo "numero de padron no encontrado"
+		fi
+    fi
+}
+
+Visualizar_log() {
+    if [ -s "$HOME/EPNro1/procesado.log" ]; then
         cat "$HOME/EPNro1/procesado.log"
-    else 
+    else
         echo "No hay datos registrados aún."
     fi
 }
 
-salir(){
-    exit
+Salir() {
+    exit 0
 }
 
 while true; do
@@ -92,11 +100,11 @@ while true; do
     read -p "Seleccione una opción del 1 al 7: " opcion 
     case $opcion in
         1) Crear_entorno ;;
-        2) correr_proceso ;;
-        3) mostrar_alumnnos ;;
-        4) mostrar_notas_altas ;;
-        5) mostrar_datos ;;
-        6) visualizar_log ;;
-        7) salir ;; 
+        2) Correr_proceso ;;
+        3) Mostrar_alumnos ;;
+        4) Mostrar_notas_altas ;;
+        5) Buscar_por_padron ;;
+        6) Visualizar_log ;;
+        7) Salir ;; 
     esac
 done
